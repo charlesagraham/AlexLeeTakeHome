a487d022e0d7927ed45e77d2fd1114e34a902eac
charl <charl@LA-MESITA>
2025-09-24 19:00:57 -0400

    Add project files.

---
+++ AlexLeeTakeHomeConsole/UtilityLibrary.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexLeeTakeHomeConsole;
+
+public class UtilityLibrary
+{
+	/// <summary>
+	/// Question 1: Write a function that takes two strings as input and returns a new string that interleaves the characters of the two input strings.
+	/// </summary>
+	public static string InterleaveStrings(string s1, string s2)
+	{
+		//We are assuming that the inputs are not the same size and will just interleave the rest of the strings
+		//If that were not the case, we would uncomment the following code
+		/*
+		if (s1.Length != s2.Length)
+		{
+			throw new ArgumentException("Strings must be the same length");
+		}
+		*/
+
+
+		//Never build string using regular strings because they are immutable
+		//We set the initial capacity if the string builder to the sum of the lengths of both strings to avoid resizing
+		var stringBuilder = new StringBuilder(s1.Length + s2.Length);
+		int index = 0;
+		for (; index < s1.Length; index++)
+		{
+			stringBuilder.Append(s1[index]);
+			if (index < s2.Length)
+			{
+				stringBuilder.Append(s2[index]);
+			}
+		}
+
+		for(; index < s2.Length; index++)
+		{
+			stringBuilder.Append(s2[index]);
+		}
+
+		return stringBuilder.ToString();
+	}
+
+	/// <summary>
+	/// Question 2: Write a function that takes a string as input and returns whether the string is a palindrome
+	/// </summary>
+	/// <param name="input"></param>
+	/// <returns></returns>
+	public static string GetIsPalendromeDisplayName(string input)
+	{
+		return IsPalendrome(input) ? "Palindrome" : "Not Palindrome";
+	}
+
+	public static bool IsPalendrome(string input)
+	{
+		if (string.IsNullOrEmpty(input))
+		{
+			return true;
+		}
+
+		int left = 0;
+		int right = input.Length - 1;
+		while (left < right)
+		{
+			if (input[left] != input[right])
+			{
+				return false;
+			}
+			left++;
+			right--;
+		}
+
+		return true;
+	}
+}
---
+++ Tests/UtilityLibraryTests.cs
@@ -0,0 +1,75 @@
+﻿using AlexLeeTakeHomeConsole;
+
+namespace Tests;
+
+public class UtilityLibraryTests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void InterleaveStrings_abc_123_returns_a1b2c3()
+	{
+		var result = UtilityLibrary.InterleaveStrings("abc", "123");
+
+		Assert.That(result, Is.EqualTo("a1b2c3"));
+	}
+
+	[Test]
+	public void InterleaveStrings_abcd_123_returns_a1b2c3d()
+	{
+		var result = UtilityLibrary.InterleaveStrings("abcd", "123");
+
+		Assert.That(result, Is.EqualTo("a1b2c3d"));
+	}
+
+	[Test]
+	public void InterleaveStrings_abc_1234_returns_a1b2c34()
+	{
+		var result = UtilityLibrary.InterleaveStrings("abc", "1234");
+
+		Assert.That(result, Is.EqualTo("a1b2c34"));
+	}
+
+	[Test]
+	public void GetIsPalendromeDisplayName_madam_Returns_Palindrome()
+	{
+		var result = UtilityLibrary.GetIsPalendromeDisplayName("madam");
+
+		Assert.That(result, Is.EqualTo("Palindrome"));
+	}
+
+	[Test]
+	public void GetIsPalendromeDisplayName_step_on_no_pets_Returns_Palindrome()
+	{
+		var result = UtilityLibrary.GetIsPalendromeDisplayName("step on no pets");
+
+		Assert.That(result, Is.EqualTo("Palindrome"));
+	}
+
+	[Test]
+	public void GetIsPalendromeDisplayName_book_Returns_Not_Palindrome()
+	{
+		var result = UtilityLibrary.GetIsPalendromeDisplayName("book");
+
+		Assert.That(result, Is.EqualTo("Not Palindrome"));
+	}
+
+	[Test]
+	public void GetIsPalendromeDisplayName_1221_Returns_Palindrome()
+	{
+		var result = UtilityLibrary.GetIsPalendromeDisplayName("1221");
+
+		Assert.That(result, Is.EqualTo("Palindrome"));
+	}
+
+	[Test]
+	public void GetIsPalendromeDisplayName_12345_Returns_Not_Palindrome()
+	{
+		var result = UtilityLibrary.GetIsPalendromeDisplayName("12345");
+
+		Assert.That(result, Is.EqualTo("Not Palindrome"));
+	}
+}
